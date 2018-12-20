400709a1bec64b1ad7e7bef12adabfa5cfa2fc42
Ali Abdullah <ali@zagaran.com>
2018-12-20 15:25:50 -0500

    Updated Course model

--- course_catalog/models.py
+++ course_catalog/models.py
@@ -3,6 +3,7 @@ from django.contrib.postgres.fields import JSONField
 
 
 class Course(models.Model):
+    course_id = models.CharField(max_length=128, unique=True)
     title = models.CharField(max_length=256)
     short_description = models.CharField(max_length=1024)
     level = models.CharField(max_length=128)
@@ -10,15 +11,10 @@ class Course(models.Model):
     language = models.CharField(max_length=128)
     platform = models.CharField(max_length=128)
     year = models.IntegerField()
-    id = models.CharField(max_length=128, unique=True)
     full_description = models.TextField(null=True)
     start_date = models.DateTimeField(null=True)
     end_date = models.DateTimeField(null=True)
     enrollment_start = models.DateTimeField(null=True)
     enrollment_end = models.DateTimeField(null=True)
     image = JSONField(null=True)
-    topics = JSONField()
-    prices = JSONField()
-    instructors = JSONField()
-    mode_types = JSONField()
     raw_json = JSONField(null=True)
