85abd91fa06e289ae5b142de88e184c0196d2251
Sean Esterkin <sean@zagaran.com>
2019-01-11 13:28:07 -0500

    Responded to comments from PR 20

--- course_catalog/tasks.py
+++ course_catalog/tasks.py
@@ -13,15 +13,11 @@ def get_edx_data():
     Task to sync mitx data with the database
     """
     url = "https://api.edx.org/catalog/v1/catalogs/248/courses"
-    while True:
+    while url:
         # print(url)
         access_token = get_access_token()
         response = requests.get(url, headers={"Authorization": "JWT " + access_token})
         for course_data in response.json()["results"]:
             parse_mitx_json_data(course_data)
 
-        # print(response.json()["next"])
-        if response.json()["next"]:
-            url = response.json()["next"]
-        else:
-            break
+        url = response.json()["next"]
