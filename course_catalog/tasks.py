844f99b0d3392de24e9b97d3469167610dc003c6
Sean Esterkin <sean@zagaran.com>
2019-01-14 18:44:06 -0500

    Move mitx url to an environment variable

--- course_catalog/tasks.py
+++ course_catalog/tasks.py
@@ -4,6 +4,7 @@ course_catalog tasks
 import requests
 from celery.task import task
 
+from course_catalog.settings import EDX_API_URL
 from course_catalog.tasks_helpers import get_access_token, parse_mitx_json_data
 
 
@@ -12,12 +13,15 @@ def get_edx_data():
     """
     Task to sync mitx data with the database
     """
-    url = "https://api.edx.org/catalog/v1/catalogs/248/courses"
+    url = EDX_API_URL
     while url:
         # print(url)
         access_token = get_access_token()
         response = requests.get(url, headers={"Authorization": "JWT " + access_token})
-        for course_data in response.json()["results"]:
-            parse_mitx_json_data(course_data)
+        if response.status_code == 429:
+            break
+        else:
+            for course_data in response.json()["results"]:
+                parse_mitx_json_data(course_data)
 
         url = response.json()["next"]
